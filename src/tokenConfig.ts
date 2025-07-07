e9611804f406cb516359e3f8efc7439712c13061
Erica Phelps <erica@blackspork.com>
2025-07-06 21:51:34 -0500

    feat:Updated hooks to ensure compatibility with AppV2 actor sheets

--- src/tokenConfig.ts
+++ src/tokenConfig.ts
@@ -1,29 +1,36 @@
 import { SpriteAnimationsConfig } from "./applications";
 
-
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-(Hooks as any).on("getHeaderControlsTokenConfig", (app: foundry.applications.sheets.TokenConfig, controls: foundry.applications.api.ApplicationV2.HeaderControlsEntry[]) => {
-  controls.push({
-    icon: "fa-solid fa-person-running",
-    label: "SPRITE-ANIMATIONS.CONFIG.HEADER",
-    onClick: () => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      void (new SpriteAnimationsConfig((app as any).actor as Actor)).render({ force: true });
-
+function getHeaderButtons(app: unknown): foundry.applications.api.ApplicationV2.HeaderControlsEntry[] {
+  return [
+    {
+      icon: "fa-solid fa-person-running",
+      label: "SPRITE-ANIMATIONS.CONFIG.HEADER",
+      class: "sprite-animations",
+      onClick: () => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        void (new SpriteAnimationsConfig((app as any).actor as Actor)).render({ force: true });
+      },
+      onclick: () => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        void (new SpriteAnimationsConfig((app as any).actor as Actor)).render({ force: true });
+      }
     }
-  });
-});
+  ]
+}
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-(Hooks as any).on("getHeaderControlsPrototypeTokenConfig", (app: foundry.applications.sheets.TokenConfig, controls: foundry.applications.api.ApplicationV2.HeaderControlsEntry[]) => {
-  controls.push({
-    icon: "fa-solid fa-person-running",
-    label: "SPRITE-ANIMATIONS.CONFIG.HEADER",
-    onClick: () => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      void (new SpriteAnimationsConfig((app as any).actor as Actor)).render({ force: true });
+function addHeaderButton(app: unknown, controls: foundry.applications.api.ApplicationV2.HeaderControlsEntry[]) {
+  controls.unshift(...getHeaderButtons(app));
+}
 
-    }
+["getHeaderControlsTokenConfig",
+  "getHeaderControlsPrototypeTokenConfig",
+  "getActorSheetHeaderButtons",
+  "getHeaderControlsActorSheetV2"
+].forEach(hook => {
+  console.log("Setting hook:", hook);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  (Hooks as any).on(hook, (app: unknown, controls: foundry.applications.api.ApplicationV2.HeaderControlsEntry[]) => {
+    console.log(hook, controls);
+    addHeaderButton(app, controls);
   });
 });
