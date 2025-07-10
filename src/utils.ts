27529f223760c8b0b02ed2c63e4ca0f8acf3afd5
Erica Phelps <erica@blackspork.com>
2025-07-10 01:36:15 -0500

    feat:Added last element queue item to remove video when playing another animation

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,8 @@ import { AnimationConfig } from "./interfaces";
 import mimeJSON from "./mime.json";
 const mimeDB = mimeJSON as Record<string, string>;
 
+const lastVideoElement: { mesh: foundry.canvas.primary.PrimarySpriteMesh, elem: HTMLVideoElement }[] = [];
+
 function applyProperPixels(mesh: foundry.canvas.primary.PrimarySpriteMesh) {
   if (game.modules?.get("proper-pixels")?.active) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
@@ -47,24 +49,61 @@ export async function playAnimations(mesh: foundry.canvas.primary.PrimarySpriteM
   await preloadTextures(...configs);
   const filters = [...mesh.filters ?? []];
 
+  const vidElements: HTMLVideoElement[] = [];
+
+  const lastIndex = lastVideoElement.findIndex(elem => elem.mesh === mesh);
+  if (lastIndex !== -1) {
+    const lastElem = lastVideoElement[lastIndex]
+    lastVideoElement.splice(lastIndex, 1);
+    lastElem.elem.remove();
+  }
+
   for (let i = 0; i < configs.length; i++) {
     const config = configs[i];
-    mesh.texture = PIXI.Texture.from(config.src);
+
+    const loop = (i < configs.length - 1 ? false : typeof config.loop == "boolean" ? config.loop : true);
+
+    if (isVideo(config.src)) {
+      const vid = document.createElement("video");
+      vidElements.push(vid);
+      vid.src = config.src;
+      vid.crossOrigin = "anonymous";
+      vid.loop = loop;
+      vid.autoplay = true;
+      vid.playsInline = true;
+      vid.style.display = "none";
+      document.body.appendChild(vid);
+      await vid.play();
+
+      const index = lastVideoElement.findIndex(elem => elem.mesh === mesh);
+      if (index !== -1) lastVideoElement[index].elem = vid;
+      else lastVideoElement.push({ mesh, elem: vid });
+
+      mesh.texture = PIXI.Texture.from(vid);
+    } else {
+      mesh.texture = PIXI.Texture.from(config.src);
+    }
 
     // Re-apply filters
     if (mesh.filters) mesh.filters.splice(0, mesh.filters.length, ...filters);
 
     applyPixelCompatibility(mesh);
 
-    const loop = (i < configs.length - 1 ? false : typeof config.loop == "boolean" ? config.loop : true);
 
     const { resource } = mesh.texture.baseTexture;
     if (resource instanceof PIXI.VideoResource) {
       const { source } = resource;
+      // Duplicate?
       source.currentTime = 0;
       await source.play();
       source.loop = loop;
       if (!loop) await animationEnd(resource);
+    }
+  }
+  if (vidElements.length > 1) {
+    for (let i = 0; i < vidElements.length - 2; i++) {
+      const elem = vidElements[i];
+      elem.remove();
     }
   }
 }
@@ -107,4 +146,12 @@ export function mimeType(path: string) {
   if (!ext) return "application/octet-stream";
   else if (mimeDB[ext]) return mimeDB[ext];
   else return "application/octet-stream";
+}
+
+export function isVideo(path: string): boolean {
+  return mimeType(path).split("/")[0] === "video";
+}
+
+export function isImage(path: string): boolean {
+  return mimeType(path).split("/")[0] === "image";
 }
